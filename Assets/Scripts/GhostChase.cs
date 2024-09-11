cafad37b73c0bf9ffb4560cc8d61b1a5ebe4e821
788465h10 <dolongnhat0301@gmail.com>
2024-09-11 11:31:57 +0700

    update ghost movement, now they can follow to pacman if they see him in range of distance

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -3,5 +3,30 @@ using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
+    private void OnDisable()
+    {
+        this.ghost.scatter.Enable();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+        if (node != null && this.enabled && !this.ghost.frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float mintoPacman = float.MaxValue;
 
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                Vector3 nextPosition = this.ghost.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.ghost.pacman.position - nextPosition).sqrMagnitude;
+
+                if (distance < mintoPacman)
+                {
+                    mintoPacman = distance;
+                    direction = availableDirection;
+                }
+            }
+            this.ghost.movement.SetDirection(direction);
+        }
+    }
 }
