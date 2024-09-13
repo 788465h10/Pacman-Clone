88f80de0ddab53db6ef6dce0dd2b66d69afd096c
788465h10 <dolongnhat0301@gmail.com>
2024-09-13 16:47:16 +0700

    update ghost in home, now they can't move out

--- Assets/Scripts/GhostHome.cs
+++ Assets/Scripts/GhostHome.cs
@@ -1,7 +1,47 @@
 
+using System.Collections;
 using UnityEngine;
 
 public class GhostHome : GhostBehavior
 {
+    public Transform home;
+    public Transform exithome;
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+    }
+    private void OnDisable()
+    {
+        StartCoroutine(Exit());
+    }
+    private IEnumerator Exit()
+    {
+        this.ghost.movement.SetDirection(Vector2.up, true);
+        this.ghost.movement.rigidbody.isKinematic = true;
+        this.ghost.movement.enabled = false;
 
+        Vector3 position = this.transform.position;
+        float duration = 0.5f;
+        float elapsed = 0.0f;
+        while(elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(position, this.home.position, elapsed / duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(this.home.position, this.exithome.position, elapsed / duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        this.ghost.movement.SetDirection(new Vector2(Random.value < 0.5f ? -1.0f : 1.0f, 0.0f), true);
+        this.ghost.movement.rigidbody.isKinematic = false;
+        this.ghost.movement.enabled = true;
+    }
 }
