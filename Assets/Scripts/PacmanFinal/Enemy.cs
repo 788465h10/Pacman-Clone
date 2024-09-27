c657473f963aad7da16c15130fcd8fb0702d715c
788465h10 <dolongnhat0301@gmail.com>
2024-09-27 22:01:22 +0700

    add chasing behavior for enemy (change moveset later)

---
+++ Assets/Scripts/PacmanFinal/Enemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject target;
+    public float speed = 10f;
+
+    private float distance;
+
+    private void Awake()
+    {
+        target = GameObject.Find("Pacman");
+    }
+
+    private void Update()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        Vector2 direction = target.transform.position - transform.position;
+        direction.Normalize();
+        
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
