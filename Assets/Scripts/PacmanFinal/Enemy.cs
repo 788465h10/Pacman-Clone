a15def16201d3802bf4bfbb26ebf8916e49b73b0
788465h10 <dolongnhat0301@gmail.com>
2024-09-30 15:24:14 +0700

    add ranged enemy

--- Assets/Scripts/PacmanFinal/Enemy.cs
+++ Assets/Scripts/PacmanFinal/Enemy.cs
@@ -17,10 +17,6 @@ public class Enemy : MonoBehaviour
         distance = Vector2.Distance(transform.position, target.transform.position);
         Vector2 direction = target.transform.position - transform.position;
         direction.Normalize();
-        
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
---
+++ Assets/Scripts/PacmanFinal/RangedWeapon.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RangedWeapon : MonoBehaviour
+{
+    public GameObject bullets;
+    public Transform initialPos;
+    public float Time2Shoot = 0.3f;
+    public float bulletPower;
+
+
+    private float time2Shoot;
+
+    private void Update()
+    {
+        time2Shoot -= Time.deltaTime;
+        if (time2Shoot < 0)
+        {
+            Shooting();
+        }
+    }
+
+    private void Shooting()
+    {
+        time2Shoot = Time2Shoot;
+
+        GameObject bullet = Instantiate(bullets, initialPos.position, Quaternion.identity);
+
+        GameObject player = GameObject.Find("Pacman");
+
+        Vector3 direction = player.transform.position - transform.position;
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(direction.x, direction.y) * bulletPower, ForceMode2D.Impulse);
+    }
+}
